87129f5e721aeb06c9c17d73aeb776480446e6a2
Alberto Romero <alberto@darksnakegames.com>
2021-09-21 22:07:54 +0200

    Add FinalIdentifier. The identifier may be changed from server if it is the same. Also, its on its final format (Simple). App will set this one in the keychain from api response.

--- Shared/UserData.cs
+++ Shared/UserData.cs
@@ -9,6 +9,8 @@ namespace Shared
         public IEnumerable<string> Roles { get; set; }
         public Dictionary<string, string> Policies { get; set; }
         public string Token { get; set; }
+        // it may have changed from ComplexIdentifier, if it already existed
+        public string FinalIdentifier { get; set; }
 
         public float CallBalance { get; set; }
         public Language Language { get; set; }
