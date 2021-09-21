cbcfb8c7707cba39271423539c8b6da968f72fcb
Alberto Romero <alberto@darksnakegames.com>
2021-09-21 22:08:29 +0200

    Enable keychain and usage tests.

---
+++ MauiPranksterApp/Native/Cross/IPManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiPranksterApp.Native.Cross
+{
+	public static class IPManager
+	{
+        public static string GetLocalAddress()
+        {
+            var IpAddress = Dns.GetHostAddresses(Dns.GetHostName()).FirstOrDefault();
+
+            if (IpAddress != null)
+                return IpAddress.ToString();
+
+            return "255.255.255.255";
+        }
+    }
+}
