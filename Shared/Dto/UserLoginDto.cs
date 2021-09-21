1d6606e6599006c2d1315a0ecdd4c89d3f93bd17
Alberto Romero <alberto@darksnakegames.com>
2021-09-21 22:05:59 +0200

    Manage server-generated identifier trough ComplexIdentifier class

--- Shared/Dto/UserLoginDto.cs
+++ Shared/Dto/UserLoginDto.cs
@@ -1,5 +1,6 @@
 using JetBrains.Annotations;
 using Shared.Enums;
+using System.Collections.Generic;
 
 namespace Shared.Dto
 {
@@ -10,10 +11,16 @@ namespace Shared.Dto
          * Android -> SIM
          * iOS -> Phone Number
          */
+        public List<string> ComplexIdentifier { get; set; }
+
+        // simpleIdentifier
         public string Identifier { get; set; }
         public Platform LastPlatform { get; set; }
 
         [CanBeNull] public string DeviceModel { get; set; }
+
+        [CanBeNull] public string OSVersion { get; set; }
+
         public string IPAddress { get; set; }
 
         // email is filled if comes from oauth
---
+++ Shared/Utils/IdentifierUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utils
+{
+	public static class IdentifierUtils
+	{
+        private static readonly Random _random = new Random();
+
+        // Generates a random number within a range.      
+        private static int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        // Generates a random string with a given size.    
+        private static string RandomString(int size, bool lowerCase = false)
+        {
+            var builder = new StringBuilder(size);
+
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):   
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.  
+
+            // char is a single Unicode character  
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length = 26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)_random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+
+        public static List<string> CreateComplexIdentifier()
+		{
+            List<string> parts = new();
+
+            for (int i = 0; i < 4; i++)
+                parts.Add(RandomNumber(1000, 9999).ToString());
+
+            string exclusivePart = RandomNumber(100, 999).ToString() + RandomString(1);
+            parts.Add(exclusivePart);
+
+           return parts;
+		}
+
+    }
+}
