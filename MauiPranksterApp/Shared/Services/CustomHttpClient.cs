e5cf4c3380291cce64cb83a91ca310cfa90275df
Alberto Romero <alberto@darksnakegames.com>
2021-09-23 03:33:27 +0200

    Add CustomHttpClient

---
+++ MauiPranksterApp/Shared/Services/CustomHttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MauiPranksterApp.Shared.Services
+{
+    public class CustomHttpClient
+    {
+        public HttpClient c { get; private set; }
+
+        public CustomHttpClient(HttpClient httpClient)
+        {
+            httpClient.Timeout = TimeSpan.FromSeconds(7);
+            httpClient.BaseAddress = new Uri("https://api.pranksterapp.com/");
+            httpClient.DefaultRequestHeaders.Add("Prankster-Platform","ios-app");
+
+            c = httpClient;
+        }
+    }
+}
