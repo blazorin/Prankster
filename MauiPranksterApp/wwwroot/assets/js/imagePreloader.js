230bf0ff51c1d62d114286ccaf23ceb471eae7f7
Alberto Romero <alberto@darksnakegames.com>
2021-10-23 14:21:36 +0200

    Add imagePreloader

---
+++ MauiPranksterApp/wwwroot/assets/js/imagePreloader.js
@@ -0,0 +1,20 @@
+﻿window.preloadImages = function (imageNames) {
+
+    var imagesArray = Array.from(imageNames);
+    imagesArray.forEach(imageName => {
+        var imagePath = 'https://cdn.pranksterapp.com/img/' + imageName;
+        if (loaded[imagePath])
+            return;
+
+        link = document.createElement('link');
+        link.rel = 'preload';
+        link.href = imagePath;
+        link.as = 'image';
+        loaded[imagePath] = true;
+
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+    });
+}
+
+loaded = [];
